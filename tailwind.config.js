f682e4cecb6cd29430b6d634ece606e16020af09
amrit raj <amritrajsingh179@gmail.com>
2025-09-10 00:35:17 +0530

    initialzation

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+content: [
+    "./index.html",              // root HTML
+    "./src/**/*.{js,ts,jsx,tsx}" // all files in src with js, ts, jsx, tsx extensions
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
