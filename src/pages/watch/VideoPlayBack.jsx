5cc3d98da00801f23a6d2f3001a9415ef8f5b34b
amrit raj <amrit.createch@gmail.com>
2025-09-19 18:08:50 +0530

    commit

--- src/pages/watch/VideoPlayBack.jsx
+++ src/pages/watch/VideoPlayBack.jsx
@@ -1,7 +1,7 @@
 import React,{useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import {useSelector,useDispatch} from 'react-redux'
-import { fetchVideos } from '../../store/slices/videoSLice';
+import { fetchVideos } from '../../store/slices/videoSlice';
 function VideoPlayBack() {
     const dispatch = useDispatch()
    useEffect(() => {
--- src/store/slices/videoSLice.js
+++ src/store/slices/videoSLice.js
@@ -2,14 +2,23 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
 
-export const fetchVideos = createAsyncThunk('fetchVideos', async ({categoryId, query}) => {
-   let url = `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_KEY}`
-    if (categoryId) url +=`&videoCategoryId=${categoryId}`;
-    if(query)  url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=50&q=${query}&key=${API_KEY}`;
-     const res = await fetch (url)
-    const data = await res.json()
-    return data.items
-})
+export const fetchVideos = createAsyncThunk(
+  'fetchVideos',
+  async ({ categoryId } = {}) => {
+    let url = `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_KEY}`;
+
+      // Add category filter if present
+      if (categoryId) {
+        url += `&videoCategoryId=${categoryId}`;
+      
+    }
+
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.items;
+  }
+);
+
 
 const videoSlice = createSlice({
     name: "videos", 
